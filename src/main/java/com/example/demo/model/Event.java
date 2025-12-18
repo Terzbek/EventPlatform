02953c4088fe5cece68d4fefb1e67b5be83f9884
Adilbek <adilbek003@mail.ru>
2025-12-18 16:57:44 +0500

    first Commit

---
+++ src/main/java/com/example/demo/model/Event.java
@@ -0,0 +1,91 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.OffsetDateTime;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private OffsetDateTime startDateTime;
+    private OffsetDateTime endDateTime;
+    private String country;
+    private String category;
+
+    public Event(){
+    }
+
+    public Event(Long id, String title, String description, OffsetDateTime startDateTime, OffsetDateTime endDateTime, String country, String category) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.country = country;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public OffsetDateTime getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(OffsetDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public OffsetDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(OffsetDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
